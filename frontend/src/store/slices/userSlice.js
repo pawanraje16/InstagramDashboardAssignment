06ce62a12e42ebc7f2518466bbdb69526aa70877
pawanraje16 <136164835+pawanraje16@users.noreply.github.com>
2025-09-29 22:10:42 +0530

    cmimit

--- frontend/src/store/slices/userSlice.js
+++ frontend/src/store/slices/userSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_BASE_URL = 'https://instagramdashboardassignment.onrender.com';
+const API_BASE_URL = 'http://localhost:8000/api';
 
 // Initial dummy state
 const initialState = {
